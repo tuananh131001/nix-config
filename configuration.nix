f9e6d71a075060f6ee12a941d5f9bf6a10764ae5
Tuan Anh <tuananh131001@gmail.com>
2025-10-04 23:41:16 +0700

    enable fish

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,7 @@
      packages = with pkgs; [
        tree
      ];
+     shell = pkgs.fish;
    };
 
   programs.firefox.enable = true;
@@ -81,12 +82,9 @@
    environment.systemPackages = with pkgs; [
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
-     neovim
-     ghostty
      chezmoi
      mise
      brave
-     git
      fish
      gh
    ];
--- home.nix
+++ home.nix
@@ -18,11 +18,28 @@ programs.fish = {
     enable = true;
   };
   home.packages = [
+    pkgs.wget
+    pkgs.neovim
+    pkgs.ghostty
     pkgs.nodejs
     pkgs.gh
     pkgs.python3
     pkgs.gcc
+    pkgs.fzf
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.htop
   ];
+
+  programs.git = {
+    enable = true;
+    userName = "anhnt";
+    userEmail = "tuananh131001@gmail.com";
+
+  };
+  programs.bash = {
+  
+  };
 
 
 }
