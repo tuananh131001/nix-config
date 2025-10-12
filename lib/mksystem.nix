9b9f840a5f5cfd1d1c2ee6d12029e8a399c7ec93
Tuan Anh <tuananh131001@gmail.com>
2025-10-12 09:41:47 +0700

    Relocate hardware

--- lib/mksystem.nix
+++ lib/mksystem.nix
@@ -37,12 +37,6 @@ in systemFunc rec {
     # Allow unfree packages.
     { nixpkgs.config.allowUnfree = true; }
 
-    # Bring in WSL if this is a WSL build
-    (if isWSL then inputs.nixos-wsl.nixosModules.wsl else {})
-
-    # Snapd on Linux
-    (if isLinux then inputs.nix-snapd.nixosModules.default else {})
-
     machineConfig
     userOSConfig
     home-manager.home-manager {
