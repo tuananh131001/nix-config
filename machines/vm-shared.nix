f6fd755ebf3854403815480bf13e1cdbaaa079be
anhnt <tuananh131001@gmail.com>
2025-10-13 12:42:00 +0700

    Perfect keyd pair with i3 and with mac-layout

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -66,22 +66,11 @@
     ];
 
   services.keyd = {
-    enable = false;
+    enable = true;
     keyboards = {
       # The name is just the name of the configuration file, it does not really matter
       default = {
-        ids = [ "*" ]; # what goes into the [id] section, here we select all keyboards
-        # Everything but the ID section:
-        settings = {
-          # The main layer, if you choose to declare it in Nix
-          main = {
-            capslock = "layer(control)"; # you might need to also enclose the key in quotes if it contains non-alphabetical symbols
-          };
-          otherlayer = {};
-        };
         extraConfig = builtins.readFile ./keyd;
-          
-        
       };
     };
 };
