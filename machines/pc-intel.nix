fd355c4ba366ad063c428d99f7f914fe3e131baa
anhnt <tuananh131001@gmail.com>
2025-10-23 21:09:39 +0700

    Fixed tearing issue

--- machines/pc-intel.nix
+++ machines/pc-intel.nix
@@ -25,11 +25,32 @@
   hardware.alsa.enablePersistence = true;
     # Nvidia
   hardware.graphics.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
-  services.xserver = {
-    # Set default screen resolution and refresh rate
+ services.xserver = {
+    videoDrivers = [ "nvidia" ];
+
+    config = ''
+      Section "Device"
+          Identifier  "Intel Graphics"
+          Driver      "intel"
+          #Option      "AccelMethod"  "sna" # default
+          #Option      "AccelMethod"  "uxa" # fallback
+          Option      "TearFree"        "true"
+          Option      "SwapbuffersWait" "true"
+          BusID       "PCI:0:2:0"
+          #Option      "DRI" "2"             # DRI3 is now default
+      EndSection
+
+      Section "Device"
+          Identifier "nvidia"
+          Driver "nvidia"
+          BusID "PCI:1:0:0"
+          Option "AllowEmptyInitialConfiguration"
+      EndSection
+    '';
     screenSection = ''
-      Option "PreferredMode" "1920x1080_100.00"
+      Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
+      Option         "AllowIndirectGLXProtocol" "off"
+      Option         "TripleBuffer" "on"
     '';
   };
   hardware.nvidia = {
@@ -46,7 +67,14 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-  boot.kernelParams = [ "snd-intel-dspcfg.dsp_driver=1" ];
+  boot = {
+    kernelParams =
+      [ "acpi_rev_override" "mem_sleep_default=deep" "intel_iommu=igfx_off" "nvidia-drm.modeset=1" ];
+    kernelPackages = pkgs.linuxPackages_5_4;
+    extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  }; # fix nvidia tearing
+  
+  # boot.kernelParams = [ "snd-intel-dspcfg.dsp_driver=1" ];
 
   # Steam
   programs.steam.enable = true;
